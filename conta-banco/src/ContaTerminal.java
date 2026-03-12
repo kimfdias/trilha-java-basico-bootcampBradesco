886e0b3f947dc1dfacbbd936bd2dc70742d12997
Kim <kimberlyfdias@gmail.com>
2026-03-12 15:47:39 -0300

    Importação Classe Scanner feita e mensagens inseridas; juntamente com os valores obtidos e mensagem final retornada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+        // TO DO: 1 - Conhecer e importar a classe Scanner
+
+        // 2. Exibir as mensagens para o usuário
+
+        // 3. Obter pela Scanner os valores digitados no terminal
+
+        // 4. Exibir a mensagem da conta criada
+
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite a agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome:");
+        scanner.nextLine(); // limpa o buffer
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + numero +
+                " e seu saldo de" + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
-} 
+}
